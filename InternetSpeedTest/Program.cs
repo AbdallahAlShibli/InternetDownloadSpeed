2819ad536bbfb10dfb5fdc5be464d5dfb90d309f
Abdullah <48906718+AbdallahAlShibli@users.noreply.github.com>
2023-06-03 16:28:51 +0400

    Add project files.

---
+++ InternetSpeedTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Net;
+
+string ss = @"
+                                                                                                                                                                
+
+        ██╗███╗░░██╗████████╗███████╗██████╗░███╗░░██╗███████╗████████╗  ░██████╗██████╗░███████╗███████╗██████╗░
+        ██║████╗░██║╚══██╔══╝██╔════╝██╔══██╗████╗░██║██╔════╝╚══██╔══╝  ██╔════╝██╔══██╗██╔════╝██╔════╝██╔══██╗
+        ██║██╔██╗██║░░░██║░░░█████╗░░██████╔╝██╔██╗██║█████╗░░░░░██║░░░  ╚█████╗░██████╔╝█████╗░░█████╗░░██║░░██║
+        ██║██║╚████║░░░██║░░░██╔══╝░░██╔══██╗██║╚████║██╔══╝░░░░░██║░░░  ░╚═══██╗██╔═══╝░██╔══╝░░██╔══╝░░██║░░██║
+        ██║██║░╚███║░░░██║░░░███████╗██║░░██║██║░╚███║███████╗░░░██║░░░  ██████╔╝██║░░░░░███████╗███████╗██████╔╝
+        ╚═╝╚═╝░░╚══╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░╚══╝╚══════╝░░░╚═╝░░░  ╚═════╝░╚═╝░░░░░╚══════╝╚══════╝╚═════╝░
+
+";
+
+
+Console.WriteLine(ss);
+
+
+var watch = new Stopwatch();
+byte[] date;
+
+using (var webClient = new WebClient()) { 
+
+watch.Start();
+    date = webClient.DownloadData("https://download.microsoft.com/download/2/7/A/27AF1BE6-DD20-4CB4-B154-EBAB8A7D4A7E/officedeploymenttool_16327-20214.exe");
+    watch.Stop();
+}
+Console.WriteLine("Download Completed!");
+var speed = Math.Round((date.Length/watch.Elapsed.TotalSeconds)/(1000*1000),2);
+Console.WriteLine($"\t\tTime Elapsad: {watch.Elapsed}");
+Console.WriteLine($"\t\tInternet Speed: {speed} Mpbs");
+
+Console.ReadKey();
